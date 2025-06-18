ac43ba651a57771b29505b6434882b9083619093
santinzz <santiagogonuz@gmail.com>
2025-06-17 18:31:20 -0600

    setup auth and db

---
+++ src/lib/db/connection.ts
@@ -0,0 +1,7 @@
+import { env } from '@/env';
+import { drizzle } from 'drizzle-orm/libsql';
+
+export const db = drizzle({ connection: {
+  url: env.TURSO_DATABASE_URL,
+  authToken: env.TURSO_AUTH_TOKEN,
+} })
